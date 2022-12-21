493c59f9531997b4a003345078f1f7e4e0b47726
Josue Herrera <joeljosue.herrerareyes@gmail.com>
2022-12-21 13:55:52 -0500

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,37 @@
+
+// 86. Inicio del proyecto - WebServer
+
+/*
+const http = require('http');
+
+http.createServer( (req, res) =>{
+
+    res.write('Viva Messi');
+    res.end();
+})
+.listen(8082);
+
+console.log('Messi casi pierde el mundial en el tiempo:', 8082);
+*/
+
+// 87. Request y Responde
+
+const http = require('http');
+
+http.createServer( (req, res) =>{
+
+    // <- Atajo para descargar el csv con un nombre que eligamos
+    res.setHeader( 'Content-Disposition', 'attachment; filename=lista.csv' ); 
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+    
+    res.write( 'id, nombre\n' );
+    res.write( '1, Josue\n' );
+    res.write( '2, Karla\n' );
+    res.write( '3, Max\n' );
+    res.write( '4, Sofia\n' );
+    res.write( '5, Alex\n' );
+    res.end();
+})
+.listen(8082);
+
+console.log('Escuchando desde el puerto:', 8082);
---
+++ app.js
@@ -0,0 +1,210 @@
+
+// 88. Introduccion a EXPRESS
+
+/*
+const express = require('express')
+const app = express()
+const port = 8082;
+
+app.get('/', function (req, res) {
+  res.send('Home Page')
+});
+
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hola Copa Mundial en su respectiva ruta')
+});
+
+app.get('*', function (req, res) {
+    res.send('404 | Page not Found')
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`)
+});
+*/
+
+// 89. Servir contenido estático
+
+/*
+const express = require('express')
+const app = express()
+const port = 8082;
+
+/Servir contenido estático
+app.use(express.static('public'));
+
+
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hola Copa Mundial en su respectiva ruta');
+});
+
+/ Funciona para redirigir una pagina estatica a un directorio que no fue declarado /
+app.get('*', function (req, res) {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`);
+});
+*/
+
+// 90. Servir un sitio web completo
+
+/*
+const express = require('express')
+const app = express()
+const port = 8082;
+
+app.use(express.static('public'));
+
+app.get('/generic', function (req, res) {
+    res.sendFile( __dirname + '/public/generic.html')
+});
+
+app.get('/elements', function (req, res) {
+    res.sendFile( __dirname + '/public/elements.html')
+});
+
+app.get('*', function (req, res) {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`);
+});
+*/
+
+// 91. Handlebars y 92. Argumentos desde el controlador
+
+/*
+const express = require('express')
+const app = express()
+const port = 8082;
+
+// TODO: require('hbs');
+app.set('view engine', 'hbs');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render( 'home', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('/generic', function (req, res) {
+    res.sendFile( __dirname + '/public/generic.html' );
+});
+
+app.get('/elements', function (req, res) {
+    res.sendFile( __dirname + '/public/elements.html' );
+});
+
+app.get('*', function (req, res) {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`);
+});
+*/
+
+// 93. Usando parciales con HBS
+
+/*
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = 8082;
+
+// Handlebars 
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/partials');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render( 'home', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('/generic', function (req, res) {
+    res.render( 'generic', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('/elements', function (req, res) {
+    res.render( 'elements', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('*', function (req, res) {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`);
+});
+*/
+
+// 94. Preparar Webserver para subirlo a un hosting
+
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
+
+// Handlebars 
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/partials');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render( 'home', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('/generic', function (req, res) {
+    res.render( 'generic', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('/elements', function (req, res) {
+    res.render( 'elements', {
+        nombre: 'Josue Herrera',
+        titulo: 'Curso de Node JS',
+        caracteristica: 'Curso de Induccion de Upnify'
+    });
+});
+
+app.get('*', function (req, res) {
+    res.sendFile( __dirname + '/public/404.html');
+});
+
+app.listen( port, () => {
+    console.log(`Escuchando desde el puerto: http://localhost:${port}`);
+});
